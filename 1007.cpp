64a65485d0ceea82235bd632d4972d2757327b31
alvinlin0824 <alvin.lin@abbott.com>
2023-07-20 14:06:54 -0700

    Commit From VS

--- 1007.cpp
+++ 1007.cpp
@@ -3,7 +3,12 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    double sum = a/1000 + a/100 + a/10 + a/1;
-    printf("%d",a);
+    if (999 < a & a < 10000)
+    {
+        int sum = a/1000 + (a/100 - (a/1000)*10) + (a/10 - (a/100)*10) + (a/1 - (a/10)*10);
+        printf("%d",sum);
+    } else {
+        printf("Out-of-Range");
+    }
     return 0;
 }
