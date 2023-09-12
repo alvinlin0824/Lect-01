cfeff913cc4095f192f2a9156d0f05d0b79e38cd
alvinlin0824 <alvin.lin@abbott.com>
2023-09-12 15:18:21 -0700

    Commit From VS

--- 3234.cpp
+++ 3234.cpp
@@ -7,47 +7,67 @@
 // ● Sum, mean, standard deviation of A, B, and C
 // If there is no such digit, please treat it as 0
 
-#include<stdio.h>
-// #include <cmath>
-// int main()
-// {
-//     int a,b;
-//     double c;
-//     scanf("%d\n",&a);
-//     scanf("%d\n",&b);
-//     scanf("%1f",&c);
+#include <iostream>
+#include <string>
+#include <cmath>
 
-//     // ones
-//     int o1 = a % 10; 
-//     int o2 = b % 10; 
-//     int o3 = (int)c % 10;
-//     // tens
-//     int t1 = (a/10) % 10;
-//     int t2 = (b/10) % 10;
-//     int t3 = ((int)c /10) % 10;
-//     // hundreds
-//     int h1 = (a/100) % 10;
-//     int h2 = (b/100) % 10;
-//     int h3 = ((int)c /100) % 10;
-//     // thousands
-//     int th1 = (a/1000) % 10;
-//     int th2 = (b/1000) % 10;
-//     int th3 = ((int)c /1000) % 10;
+using namespace std;
 
-//     // float d1 = sqrt(pow(x1-x2,2) + pow(y1-y2,2));
-//     // float d2 = sqrt(pow(x1-x3,2) + pow(y1-y3,2));
-//     // float d3 = sqrt(pow(x3-x2,2) + pow(y3-y2,2));
-//     // float s =  (d1+d2+d3)/2;
-//     // float area = sqrt(s*(s-d1)*(s-d2)*(s-d3));
+double sum(int A, int B, double C){
+       return A + B + C;
+} 
 
-//     // printf("%d\n",a);
-//     // printf("%d\n",b);
-//     // printf("%.1f\n ",c);
+double mean(int A, int B, double C){
+       return double(A + B + C)/3;
+} 
 
-//     printf("%d\n",t1);
-//     printf("%d\n",t2);
-//     printf("%.1f\n ",t3);
+double sd(int A, int B, double C){
+       return sqrt(((pow(A,2) + pow(B,2) + pow(C,2)) - 3*pow((A+B+C)/3,2))/3);
+} 
 
-//     return 0;
-// }
+
+int main()
+{
+    int a,b;
+    double c;
+    
+    cin >> a >> b >> c;
+  
+    cout << sum(a,b,c) << endl;
+    cout << mean(a,b,c) << endl;
+    cout << sd(a,b,c) << endl;
+
+    // ones
+    // int o1 = a % 10; 
+    // int o2 = b % 10; 
+    // int o3 = (int)c % 10;
+    // // tens
+    // int t1 = (a/10) % 10;
+    // int t2 = (b/10) % 10;
+    // int t3 = ((int)c /10) % 10;
+    // // hundreds
+    // int h1 = (a/100) % 10;
+    // int h2 = (b/100) % 10;
+    // int h3 = ((int)c /100) % 10;
+    // // thousands
+    // int th1 = (a/1000) % 10;
+    // int th2 = (b/1000) % 10;
+    // int th3 = ((int)c /1000) % 10;
+
+    // float d1 = sqrt(pow(x1-x2,2) + pow(y1-y2,2));
+    // float d2 = sqrt(pow(x1-x3,2) + pow(y1-y3,2));
+    // float d3 = sqrt(pow(x3-x2,2) + pow(y3-y2,2));
+    // float s =  (d1+d2+d3)/2;
+    // float area = sqrt(s*(s-d1)*(s-d2)*(s-d3));
+
+    // printf("%d\n",a);
+    // printf("%d\n",b);
+    // printf("%.1f\n ",c);
+
+    // printf("%d\n",t1);
+    // printf("%d\n",t2);
+    // printf("%.1f\n ",t3);
+
+    return 0;
+}
 
